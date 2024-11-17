d1bbc35cbf60946eda80cb7973f4f215db598186
Usu <usuedeaghe@gmail.com>
2024-11-17 22:01:11 +0000

    Model Set up

---
+++ src/main/java/com/gadgetreview/api/models/Gadget.java
@@ -0,0 +1,9 @@
+package com.gadgetreview.api.models;
+
+public class Gadget {
+    private Long id;
+    private String name;
+    private String brand;
+    private String category;
+    private float price;
+}
---
+++ src/main/java/com/gadgetreview/api/models/Review.java
@@ -0,0 +1,8 @@
+package com.gadgetreview.api.models;
+
+public class Review {
+    private Long id;
+    private String title;
+    private String content;
+    private int start;
+}
